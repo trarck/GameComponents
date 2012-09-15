3536884dd09f9f8243527c6e7f391d60b395aa7d
trarck <trarck@gmail.com>
2012-09-16 00:24:28 +0800

    base Component

---
+++ Classes/Components/Component.h
@@ -0,0 +1,12 @@
+#ifndef __Component_H__
+#define __Component_H__
+
+#include "cocos2d.h"
+
+class Component : public cocos2d::CCLayer
+{
+public:
+    virtual bool init();  
+};
+
+#endif // __Component_H__
