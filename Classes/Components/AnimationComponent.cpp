4a1a59983806a03426fe369700edc4defe8736a6
trarck <trarck@gmail.com>
2012-09-19 01:09:50 +0800

    temp

--- Classes/Components/AnimationComponent.cpp
+++ Classes/Components/AnimationComponent.cpp
@@ -1,8 +1,10 @@
 #include "AnimationComponent.h"
 
+
 NS_CC_BEGIN
 
 AnimationComponent::AnimationComponent()
+:m_animations(NULL)
 {
 	CCLOG("AnimationComponent create");
 }
@@ -10,6 +12,127 @@ AnimationComponent::AnimationComponent()
 AnimationComponent::~AnimationComponent()
 {
 	CCLOG("AnimationComponent destroy");
+	CC_SAFE_RELEASE(m_animations);
 }
+
+
+bool AnimationComponent::init()
+{
+	m_animations=new CCDictionary();
+	return true;
+}
+
+bool initWithConfigFile(const std::string& file)
+{
+	init();
+	return true;
+}
+
+-(id) initWithConfigData(CCDictionary* data)
+{
+	init();
+	return true;
+}
+
+CCObject* animationForName(const std::string& name)
+{
+	return m_animations->objectForKey(name);
+}
+
+CCAction* actionForName(const std::string& name)
+{
+	return m_animations->objectForKey(name);
+}
+
+CCAction* actionForName(const std::string& name ,int index)
+{
+	return ((CCArray*)(m_animations->objectForKey(name)))->objectAtIndex(index);
+}
+
+CCAction* actionForName(const std::string& name ,const std::string& key)
+{
+	return ((CCDictionary*)(m_animations->objectForKey(name)))->objectForKey(key);
+}
+
+CCArray* actionListForName(const std::string& name)
+{
+	return (CCArray*)(m_animations->objectForKey(name));
+}
+
+CCDictionary* actionMapForName(const std::string& name)
+{
+	return (CCDictionary*)m_animations->objectForKey(name);
+}
+
+void addAnimation(CCObject* animation,const std::string&  name)
+{
+	m_animations->setObject(animation ,name);
+}
+
+void addAction(CCAction* action ,const std::string& name)
+{
+	m_animations->setObject(action ,name);
+}
+
+void addActionList(CCArray* actionList ,const std::string& name)
+{
+	m_animations->setObject(actionList ,name);
+}
+
+void addActionMap(CCDictionary* actionMap ,const std::string& name)
+{
+	m_animations->setObject(actionMap ,name);
+}
+
+void removeAnimationNamed(const std::string& name)
+{
+	m_animations->removeObjectForKey(name);
+}
+
+CCArray* eightDirectionActionListWithFile(const char* file ,int frameCount ,CCSize frameSize ,float delay)
+{
+	CCTexture2D *texture=CCTextureCache::sharedTextureCache()->addImage(file);
+	
+	CCArray* actions=new CCArray();
+	actions->initWithCapacity(8);
+	//move action
+	for (int i=0; i<8; i++) {
+		CCArray* animFrames=new CCArray();
+		animFrames->initWithCapacity(8);
+		for (int j=0; j<frameCount; j++) {
+			CCSpriteFrame *frame=CCSpriteFrame::createWithTexture(texture ,CCRectMake(j*frameSize.width, i*frameSize.height, frameSize.width, frameSize.height));
+			animFrames->addObject(frame);
+		}
+		CCAnimation *animation=CCAnimation::createWithSpriteFrames(animFrames,delay);
+		CCAnimate *animate= CCAnimate::create(animation);
+		CCSequence *seq=CCSequence::create(animate,NULL);
+		actions->addObject(CCRepeatForever::create(seq));
+		animFrames->release();
+	}
+	return actions;
+}
+
+CCArray* eightDirectionActionListWithDir(const char* dir ,int frameCount ,CCSize frameSize ,float delay)
+{
+	CCArray *actions=new CCArray();
+	actions->initWithCapacity(8);
+	
+	//move action
+	char str[255] = {0};
+	for (int i=0; i<8; i++) {
+		CCArray* animFrames=new CCArray();
+		animFrames->initWithCapacity(frameCount);
+		for (int j=0; j<frameCount; j++) {
+			sprintf(str,"%s/%02d%03d.png",dir,i,j);
+			animation->addSpriteFrameWithFileName(str);
+		}
+		//animation.delay=delay;
+		CCAnimate *animate=[CCAnimate actionWithDuration:frameCount*delay animation:animation restoreOriginalFrame:YES];
+		CCSequence *seq=[CCSequence actions:animate,nil];
+		[actions addObject:[CCRepeatForever actionWithAction:seq]];
+	}
+	return actions;
+}
+
 
 NS_CC_END
--- Classes/Components/AnimationComponent.h
+++ Classes/Components/AnimationComponent.h
@@ -24,24 +24,24 @@ public:
 	CCDictionary* animations();
 	void animations(CCDictionary* animations);
 
-	bool initWithConfigFile(CCString*  file);
+	bool initWithConfigFile(const std::string&  file);
 
-	CCObject* animationForName(CCString* name);
-	CCAction* actionForName(CCString* name);
-	CCAction* actionForName(CCString* name ,int index);
-	CCAction* actionForName(CCString* name ,CCString* key);
+	CCObject* animationForName(const std::string& name);
+	CCAction* actionForName(const std::string& name);
+	CCAction* actionForName(const std::string& name ,int index);
+	CCAction* actionForName(const std::string& name ,const std::string& key);
 
-	CCArray* actionListForName(CCString* name);
-	CCDictionary* actionMapForName(CCString* name);
+	CCArray* actionListForName(const std::string& name);
+	CCDictionary* actionMapForName(const std::string& name);
 
-	void addAction(CCAction* action, CCString* name);
-	void addActionList(CCArray* actionList ,CCString* name);
-	void addActionMap(CCDictionary* actionMap,CCString* name);
-	void addAnimation(CCObject* animation ,CCString* name);
-	void removeAnimationNamed(CCString* name);
+	void addAction(CCAction* action, const std::string& name);
+	void addActionList(CCArray* actionList ,const std::string& name);
+	void addActionMap(CCDictionary* actionMap,const std::string& name);
+	void addAnimation(CCObject* animation ,const std::string& name);
+	void removeAnimationNamed(const std::string& name);
 
-	static CCArray* eightDirectionActionListWithFile(CCString* file ,int frameCount ,CGSize frameSize ,float delay);
-	static CCArray* eightDirectionActionListWithDir(CCString* file ,int frameCount ,CGSize frameSize ,float delay);
+	static CCArray* eightDirectionActionListWithFile(const char* file ,int frameCount ,CCSize frameSize ,float delay);
+	static CCArray* eightDirectionActionListWithDir(const char* file ,int frameCount ,CCSize frameSize ,float delay);
 private:
 	CCDictionary* m_animations;
 };
